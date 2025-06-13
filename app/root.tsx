93ea0140591f91138eeccf54ec34522cd8e669c3
Karim Cherrab <ma.cherrab@gmail.com>
2025-06-13 21:28:36 +0100

    Update content

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
+           <title>الجيل الذهبي kids</title>
+
+        {/* <Meta /> */}
         <Links />
       </head>
       <body>
